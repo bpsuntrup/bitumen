235d3d9763a7a690db9deeb8ece77431ade9ba25
Benjamin Suntrup <benjamin.p.suntrup@gmail.com>
2025-08-30 16:21:42 -0500

    add enddate

--- app/javascript/components/EventForm.jsx
+++ app/javascript/components/EventForm.jsx
@@ -6,27 +6,55 @@ import 'react-calendar/dist/Calendar.css';
 export default function EventForm({addEvent, events, apptDate}) {
 
     let [startTime, setStartTime] = useState();
+    let [endTime, setEndTime] = useState();
 
     const startTimes = timeRange(apptDate, '08:00', '19:30', [30, 'm']) // TODO: filter unavailable times
     const endTimes = timeRange(apptDate, '08:30', '20:00', [30, 'm'])
 
-    let options = startTimes.map((t) => {
+    let startOptions = startTimes.map((t) => {
         let hhmm = t.format('hh:mm A z');
         let HHmm = t.format('HH:mm');
         return <option value={HHmm}> {hhmm} </option>
     })
-    options.unshift(<option value=""> {"Select a Time"} </option>);
+    startOptions.unshift(<option value=""> {"Select a Time"} </option>);
+
+    let endOptions = endTimes.map((t) => {
+        let hhmm = t.format('hh:mm A z');
+        let HHmm = t.format('HH:mm');
+        let duration = showDiff(startTime, t);
+        if (duration > 0) {
+            return <option value={HHmm}> {`${hhmm} (+${duration} hrs)` } </option>
+        }
+    }).filter(x => x);
+
+    endOptions.unshift(<option value=""> {"Select a Time"} </option>);
 
     return (<>
         <label> Start Time: </label> 
         <select onChange={(e) => {setStartTime(newTime(apptDate, e.target.value))}}>
-            {options}
+            {startOptions}
         </select>
+        { startTime ? 
+            <>
+        <label> End Time: </label>
+        <select onChange={(e) => {setEndTime(newTime(apptDate, e.target.value))}}>
+            {endOptions}
+        </select></>
+        :  ""}
         <div> {apptDate.toDateString()} </div>
         <div> {startTime ? startTime.format('YYYY MM dd hh:mm A z') : "select a time"} </div>
-        <input type="time" step="30" min="08:00" max="20:00"/>
     </>)
 }
+
+function showDiff(startdate, enddate) {
+    if (!startdate || !enddate) {
+        return undefined;
+    }
+    const diff =  moment.duration(enddate.diff(startdate))
+    console.log(diff)
+    return diff.asHours()
+}
+
 
 function newTime(date, HHmm) {
     if (!HHmm) {
